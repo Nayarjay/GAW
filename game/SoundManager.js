ed018d1815eae86d215ccc36624fb7c9f65bf68c
Nayarjay <74157421+Nayarjay@users.noreply.github.com>
2024-05-27 23:22:47 +0200

    add volume slideBar

--- game/SoundManager.js
+++ game/SoundManager.js
@@ -1,17 +1,23 @@
 class SoundManager {
-    constructor(scene,  musicName) {
+    constructor(scene, musicName) {
         this.scene = scene;
-
         this.musicName = musicName;
         this.musicSound = null;
+        this.volumeSlider = null; // New property to keep track of the slider element
+
+         // Initialize the slider when the scene and slider are ready
+         document.addEventListener('DOMContentLoaded', () => {
+            this.initVolumeSlider("volumeSlider");
+        });
     }
 
     initMusic() {
         const music = () => {
-            this.musicSound = new BABYLON.Sound("", "./models/Music/"+this.musicName, this.scene, null, {
+            this.musicSound = new BABYLON.Sound("", "./models/Music/" + this.musicName, this.scene, null, {
                 loop: true,
-                autoplay: true,
+                autoplay: true
             });
+            this.updateVolume(); // Ensure volume is set according to the slider at initialization
         };
 
         window.addEventListener('mousemove', () => {
@@ -25,6 +31,19 @@ class SoundManager {
         }, { once: true });
     }
 
+    initVolumeSlider(sliderId) {
+        this.volumeSlider = document.getElementById(sliderId);
+        this.volumeSlider.addEventListener('input', () => {
+            this.updateVolume();
+        });
+    }
+
+    updateVolume() {
+        if (this.musicSound && this.volumeSlider) {
+            this.musicSound.setVolume(this.volumeSlider.value / 100);
+        }
+    }
+
     stopMusic() {
         if (this.musicSound) {
             this.musicSound.stop();
@@ -32,17 +51,17 @@ class SoundManager {
     }
 
     destroy() {
-        // Arrête la musique avant de détruire l'instance
         this.stopMusic();
-        
-        // Supprime l'écouteur d'événement
         window.removeEventListener('mousemove', this.clickHandler);
-
-        // Réinitialise les propriétés à null ou undefined pour faciliter la libération de la mémoire par le garbage collector
+        if (this.volumeSlider) {
+            this.volumeSlider.removeEventListener('input', this.updateVolume);
+        }
         this.scene = null;
         this.musicName = null;
         this.musicSound = null;
         this.clickHandler = null;
+        this.volumeSlider = null;
     }
 }
+
 export default SoundManager;
